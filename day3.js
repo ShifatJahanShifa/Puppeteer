efeb215fd264b1021e27cb5e9b52357ec0e4c655
ShifatJahanShifa <bsse1301@iit.du.ac.bd>
2024-02-06 20:27:49 +0600

    added

--- day3.js
+++ day3.js
@@ -2,6 +2,7 @@ const puppeteer = require('puppeteer');
 const request = require('request');
 global.MSInputMethodContext = {};
 const cheerio = require('cheerio');
+const axios=require('axios');
 // const { URL } = require('url'); 
 // const { index } = require('cheerio/lib/api/traversing');
 
@@ -14,6 +15,17 @@ async function getHTMLSourceCode(url) {
     return html;
 }
 
+async function fetchCSS(url)
+{
+    try{
+        const response=await axios.get(url);
+        return response.data;
+    }
+    catch(error){
+        console.log("ERROR FETCHING CSS");
+        return null;
+    }
+}
 
 // the url
 const url = 'https://www.shafaetsplanet.com';
@@ -23,15 +35,27 @@ const url = 'https://www.shafaetsplanet.com';
     //console.log(htmlSourceCode);
     const $ = cheerio.load(htmlSourceCode);
 
-    //extract external css link
+    //extract css link
     const externalCSSLinks = [];
 
     $('link[rel="stylesheet"]').each((index, element) => {
         externalCSSLinks.push($(element).attr('href'));
     });
 
-    console.log(externalCSSLinks);
+    console.log(externalCSSLinks); 
 
+    //inline css
+    const inlineCSS=[];
+    $('style').each((index,element)=>{
+        inlineCSS.push($(element).text());
+    }); 
+
+    //console.log(inlineCSS);
+
+    for(let i=0;i<externalCSSLinks.length;i++){
+    const externalCSSContent=await fetchCSS(externalCSSLinks[0]);
+    console.log("extrenal css= "+externalCSSContent);
+    }
 })();
 
 
